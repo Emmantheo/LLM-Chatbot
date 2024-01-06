3c8ed47788e6ee33481203f0e78e542a9aface82
Emmantheo <emmauloko@gmail.com>
2024-01-06 21:47:46 +0100

    creating flask script

---
+++ flask.py
@@ -0,0 +1,13 @@
+from langchain.schema import HumanMessage, SystemMessage, AIMessage
+from langchain.chat_models import ChatOpenAI
+from langchain.chains import LLMChain
+from langchain.prompts import PromptTemplate
+from langchain.agents import initialize_agent, AgentType, load_tools
+from langchain.llms import OpenAI
+from flask import Flask, render_template, request, jsonify
+from dotenv import load_dotenv
+import os
+
+
+app = Flask(__name__)
+
