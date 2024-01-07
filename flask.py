afa5cfaeb6b5e01d42db6eed029e9021312c70a9
Emmantheo <emmauloko@gmail.com>
2024-01-07 14:18:28 +0100

    building chat application

--- flask.py
+++ flask.py
@@ -11,11 +11,37 @@ import os
 
 app = Flask(__name__)
 
-@app.route('/')
-def index():
-    return render_template('index.html')
-
 # Loading environment variables from .env file
 load_dotenv()
+api_key = os.getenv("OPEN_AI_KEY")
+
+#initialising the chat
+chat = ChatOpenAI(api_key=api_key, temperature=0.5)
+
+#initializing the flow
+flow= [SystemMessage(content="You are are a financial adviser AI assistant")]
+
+def get_openai_response(question):
+    flow.append(HumanMessage(content=question))
+    answer=chat(flow)
+    flow.append(AIMessage(content=answer.content))
+
+    return answer.content
+
+
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+@app.route('/chat', methods=['POST'])
+def chat():
+    input = request.form['input_text']
+    response = get_openai_response(input)
+
+    return render_template('index.html', input_text=input, response=response, flow=flow)
+
+if __name__ == '__main__':
+    app.run(debug=True)
+
 
 
