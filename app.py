d96ca8d4aecfd0f5996598581668f452c98d652e
Emmantheo <emmauloko@gmail.com>
2024-01-08 23:16:48 +0100

    app update

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ from langchain.schema import HumanMessage, SystemMessage, AIMessage
 from langchain.chat_models import ChatOpenAI
 from langchain.chains import LLMChain
 from langchain.prompts import PromptTemplate
+from langchain.agents import initialize_agent, AgentType, load_tools
 from langchain.llms import OpenAI
 from flask import Flask, render_template, request, jsonify
 import streamlit as st
@@ -28,17 +29,9 @@ if 'flow' not in st.session_state:
 #@st.cache_resource
 def get_openai_response(question):
     st.session_state["flow"].append(HumanMessage(content=question))
-    # Check if the user query is related to financial advisory
-    # if "investment options" in question.lower():
-    #     answer = financial_advisory_data["faq"]["investment_planning"]
-    # elif "risk tolerance" in question.lower():
-    #     answer = financial_advisory_data["faq"]["risk_management"]
-    # elif "retirement plans" in question.lower():
-    #     answer = financial_advisory_data["faq"]["retirement_planning"]
-
-    # else:
     answer = chat(st.session_state['flow'])
     st.session_state["flow"].append(AIMessage(content=answer.content))
+    
     return answer.content
 
 # User input
@@ -49,6 +42,7 @@ submit = st.button("Ask me")
 if submit:
     # Get response
     response = get_openai_response(input)
+    
 
     # Display response
     #st.text(f"AI: {response}")
@@ -69,44 +63,44 @@ with st.sidebar:
         #st.success("Chat history cleared!", icon="🚨")
         st.stop()
 
-# CSS to style the chat layout
-# st.markdown(
-#     """
-#     <style>
-#         .chat-container {
-#             display: flex;
-#             flex-direction: row;
-#             justify-content: space-between;
-#             align-items: flex-start;
-#         }
+#CSS to style the chat layout
+st.markdown(
+    """
+    <style>
+        .chat-container {
+            display: flex;
+            flex-direction: row;
+            justify-content: space-between;
+            align-items: flex-start;
+        }
 
-#         .user-chat {
-#             background-color: #aa00aa;  /* Light red for user chat */
-#             padding: 8px;
-#             border-radius: 8px;
-#             margin-bottom: 8px;
-#             max-width: 70%;
-#             align-self: flex-end;
-#         }
+        .user-chat {
+            background-color: #aa00aa;  /* Light red for user chat */
+            padding: 8px;
+            border-radius: 8px;
+            margin-bottom: 8px;
+            max-width: 70%;
+            align-self: flex-end;
+        }
 
-#         .ai-chat {
-#             background-color: #007acc;  /* Lighter blue for AI chat */
-#             padding: 8px;
-#             border-radius: 8px;
-#             margin-bottom: 8px;
-#             max-width: 70%;
-#             align-self: flex-start;
-#         }
-#     </style>
-#     """,
-#     unsafe_allow_html=True,
-# )
+        .ai-chat {
+            background-color: #007acc;  /* Lighter blue for AI chat */
+            padding: 8px;
+            border-radius: 8px;
+            margin-bottom: 8px;
+            max-width: 70%;
+            align-self: flex-start;
+        }
+    </style>
+    """,
+    unsafe_allow_html=True,
+)
 
-# # Display messages with the new layout
-# st.markdown('<div class="chat-container">', unsafe_allow_html=True)
-# for message in st.session_state['flow']:
-#     if isinstance(message, HumanMessage):
-#         st.markdown(f'<div class="user-chat">{message.content}</div>', unsafe_allow_html=True)
-#     elif isinstance(message, AIMessage):
-#         st.markdown(f'<div class="ai-chat">{message.content}</div>', unsafe_allow_html=True)
-# st.markdown('</div>', unsafe_allow_html=True)
+# Display messages with the new layout
+st.markdown('<div class="chat-container">', unsafe_allow_html=True)
+for message in st.session_state['flow']:
+    if isinstance(message, HumanMessage):
+        st.markdown(f'<div class="user-chat">{message.content}</div>', unsafe_allow_html=True)
+    elif isinstance(message, AIMessage):
+        st.markdown(f'<div class="ai-chat">{message.content}</div>', unsafe_allow_html=True)
+st.markdown('</div>', unsafe_allow_html=True)
